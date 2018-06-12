bfc1e4778179f8c1aa02442923a8586c33cb9b25
loftywaif002 <deep.row4@gmail.com>
2018-06-12 01:10:49 -0700

    recycler-view-added

--- app/src/main/java/com/app/cinema/cinema/Dashboard.java
+++ app/src/main/java/com/app/cinema/cinema/Dashboard.java
@@ -2,10 +2,19 @@ package com.app.cinema.cinema;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Window;
 import android.view.WindowManager;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
 public class Dashboard extends AppCompatActivity {
+
+    @BindView(R.id.recycled_movie_grid)
+    RecyclerView movie_grid_recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,10 @@ public class Dashboard extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
+        ButterKnife.bind(this);
+
+        //Define recyclerView Layout
+        movie_grid_recyclerView.setLayoutManager(new GridLayoutManager(this, getResources()
+                .getInteger(R.integer.number_of_grid_columns)));
     }
 }
